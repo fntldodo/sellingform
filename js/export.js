6522b8ad87b7006ec9257b1934fb1d235407753d
fntldodo <96985529+fntldodo@users.noreply.github.com>
2025-12-31 00:19:43 +0900

    Update export.js

--- js/export.js
+++ js/export.js
@@ -12,6 +12,7 @@
     const scripts = [
         '[cdnjs.cloudflare.com](https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js&#39;)
         '[cdnjs.cloudflare.com](https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js&#39;)
+
     ];
 
 let scriptsLoaded = 0;
